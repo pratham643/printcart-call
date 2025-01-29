abb73abd4da4c36e908b82806faada8ed73aef6a
Prathmesh Ramesh Gangarde <116508620+pratham643@users.noreply.github.com>
2025-01-29 22:19:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,22 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Screen Share - Share Your Screen Instantly",
-    description: "Share your screen instantly with anyone using a simple room code. No downloads or sign-ups required.",
-    keywords: "screen sharing, webrtc, online screen share, browser screen sharing, free screen sharing"
+    title: "PrintCart GD Connection Portal",
+    description: "Make Your Design With The Help Of GD",
+    keywords: "Designing, Live, Graphic Designer, Connection, Instant Connection"
 } satisfies Metadata;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return 
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <main className="flex flex-col justify-between min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
+                    {children}
+                   
+                </main>
+                <Clarity />
+                <Toaster />
+            </body>
+        </html>
+    );
 }
